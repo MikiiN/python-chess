b956ea53cd70d6f9eef17f535fc46df59a7cdef4
Michal Zatecka <michal.zatecka@seznam.cz>
2025-02-18 15:06:58 +0100

    basic board functions

--- src/pieces.py
+++ src/pieces.py
@@ -1,68 +1,88 @@
-class __Piece:
+import enum
+
+class PieceType(enum.Enum):
+    WHITE = True
+    BLACK = False
+
+
+class Piece:
     def __init__(
         self,
         x: int,
-        y: int
+        y: int,
+        p_type: PieceType
     ):    
         self.x = x
         self.y = y
+        self.type = p_type
+    
+    
+    def move(self, new_x, new_y):
+        self.x = new_x
+        self.y = new_y
+    
+
+
+class King(Piece):
+    def __init__(
+        self,
+        x: int,
+        y: int,
+        p_type: PieceType
+    ):
+        super().__init__(x, y, p_type)
+
+
+
+class Queen(Piece):
+    def __init__(
+        self,
+        x: int,
+        y: int,
+        p_type: PieceType
+    ):
+        super().__init__(x, y, p_type)
+
+
+
+class Bishop(Piece):
+    def __init__(
+        self,
+        x: int,
+        y: int,
+        p_type: PieceType
+    ):
+        super().__init__(x, y, p_type)
         
 
 
-class King(__Piece):
+class Knight(Piece):
     def __init__(
         self,
         x: int,
-        y: int
+        y: int,
+        p_type: PieceType
     ):
-        super().__init__(x, y)
-
-
-
-class Queen(__Piece):
-    def __init__(
-        self,
-        x: int,
-        y: int
-    ):
-        super().__init__(x, y)
-
-
-
-class Bishop(__Piece):
-    def __init__(
-        self,
-        x: int,
-        y: int
-    ):
-        super().__init__(x, y)
+        super().__init__(x, y, p_type)
         
 
 
-class Knight(__Piece):
+class Rook(Piece):
     def __init__(
         self,
         x: int,
-        y: int
+        y: int,
+        p_type: PieceType
     ):
-        super().__init__(x, y)
+        super().__init__(x, y, p_type)
         
 
 
-class Rook(__Piece):
+class Pawn(Piece):
     def __init__(
         self,
         x: int,
-        y: int
+        y: int,
+        p_type: PieceType
     ):
-        super().__init__(x, y)
-        
-
-
-class Pawn(__Piece):
-    def __init__(
-        self,
-        x: int,
-        y: int
-    ):
-        super().__init__(x, y)
+        super().__init__(x, y, p_type)
