5a0c048bc5c274f875cbcfb386ab67b33d789f56
Michal Zatecka <michal.zatecka@seznam.cz>
2025-02-18 16:58:15 +0100

    add pieces moves functions

--- src/pieces.py
+++ src/pieces.py
@@ -5,7 +5,11 @@ class PieceType(enum.Enum):
     BLACK = False
 
 
+
 class Piece:
+    MAX_POS = 7
+    MIN_POS = 0
+    
     def __init__(
         self,
         x: int,
@@ -18,8 +22,23 @@ class Piece:
     
     
     def move(self, new_x, new_y):
-        self.x = new_x
-        self.y = new_y
+        if self.MIN_POS <= new_x <= self.MAX_POS \
+            and self.MIN_POS <= new_y <= self.MAX_POS: 
+            self.x = new_x
+            self.y = new_y
+        else:
+            raise RuntimeError("Invalid position")
+    
+    
+    # get all possible piece moves (moves against game logic too)
+    def _get_all_moves(self, shifts: list[tuple[int, int]]):
+        result = []
+        for shift in shifts:
+            pos = (self.x + shift[0], self.y + shift[1])
+            if self.MIN_POS <= pos[0] <= self.MAX_POS \
+                and self.MIN_POS <= pos[1] <= self.MAX_POS:
+                result.append(pos)
+        return result
     
 
 
@@ -31,6 +50,14 @@ class King(Piece):
         p_type: PieceType
     ):
         super().__init__(x, y, p_type)
+        self._SHIFTS = [
+            (-1, -1), (1, -1), (-1, 1), (1, 1), 
+            (1, 0), (-1, 0), (0, 1), (0, -1)
+        ]
+    
+        
+    def get_all_moves(self):
+        self._get_all_moves(self._SHIFTS)
 
 
 
@@ -42,6 +69,21 @@ class Queen(Piece):
         p_type: PieceType
     ):
         super().__init__(x, y, p_type)
+        self._SHIFTS = []
+        # store all possible shifts
+        for i in range(8):
+            self._SHIFTS.append((i, i))
+            self._SHIFTS.append((-i, -i))
+            self._SHIFTS.append((i, -i))
+            self._SHIFTS.append((-i, i))
+            self._SHIFTS.append((i, 0))
+            self._SHIFTS.append((-i, 0))
+            self._SHIFTS.append((0, i))
+            self._SHIFTS.append((0, -i))
+    
+    
+    def get_all_moves(self):
+        self._get_all_moves(self._SHIFTS)
 
 
 
@@ -53,7 +95,18 @@ class Bishop(Piece):
         p_type: PieceType
     ):
         super().__init__(x, y, p_type)
-        
+        self._SHIFTS = []
+        # store all possible shifts
+        for i in range(8):
+            self._SHIFTS.append((i, i))
+            self._SHIFTS.append((-i, -i))
+            self._SHIFTS.append((i, -i))
+            self._SHIFTS.append((-i, i))
+    
+       
+    def get_all_moves(self):
+        self._get_all_moves(self._SHIFTS)
+    
 
 
 class Knight(Piece):
@@ -64,6 +117,14 @@ class Knight(Piece):
         p_type: PieceType
     ):
         super().__init__(x, y, p_type)
+        self._SHIFTS = [
+            (-2, -1), (2, -1), (-2, 1), (2, 1),
+            (1, 2), (1, -2), (-1, 2), (-1, -2)
+        ]
+    
+        
+    def get_all_moves(self):
+        return self._get_all_moves(self._SHIFTS)
         
 
 
@@ -75,6 +136,17 @@ class Rook(Piece):
         p_type: PieceType
     ):
         super().__init__(x, y, p_type)
+        self._SHIFTS = []
+        # store all possible shifts
+        for i in range(8):
+            self._SHIFTS.append((i, 0))
+            self._SHIFTS.append((-i, 0))
+            self._SHIFTS.append((0, i))
+            self._SHIFTS.append((0, -i))
+    
+        
+    def get_all_moves(self):
+        self._get_all_moves(self._SHIFTS)
         
 
 
@@ -86,3 +158,10 @@ class Pawn(Piece):
         p_type: PieceType
     ):
         super().__init__(x, y, p_type)
+        self._SHIFTS = [
+            (1, -1), (1, 1), (1, 0), (2, 0)
+        ]
+    
+        
+    def get_all_moves(self):
+        self._get_all_moves(self._SHIFTS)
