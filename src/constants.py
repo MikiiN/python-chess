f4b817d7cea3fe8ec3e825729eefc09c0223d239
Michal Žatečka <zateckamichal8@gmail.com>
2025-09-16 17:30:06 +0200

    begin of chess board

---
+++ src/constants.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class GameMode(Enum):
+        ONE_PLAYER = 0
+        TWO_PLAYERS = 1
+        
+
+
+class PlayerColor(Enum):
+        WHITE = 0
+        BLACK = 1
--- src/view/main_window.py
+++ src/view/main_window.py
@@ -1,17 +1,19 @@
 from PySide6.QtWidgets import QMainWindow
 
+from src.constants import GameMode, PlayerColor
 from src.view.ui.main_window_ui import Ui_MainWindow
 from src.view.menu_widget import MenuWidget
 from src.view.choose_side_widget import ChooseSideWidget
 
 
-class MainWindow(QMainWindow):
+class MainWindow(QMainWindow):    
     def __init__(self):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.setWindowTitle("Chess Game")
         self.chosen_game_mode = None
+        self.chosen_color = None
         self.widget_dict = {}
         self.widget_list = []
         self._init_widgets()
@@ -32,8 +34,19 @@ class MainWindow(QMainWindow):
             
     
     def one_player_clicked(self):
+        self.chosen_game_mode = GameMode.ONE_PLAYER
         self.ui.stackedWidget.setCurrentIndex(1)
         
     
     def two_player_clicked(self):
+        self.ui.stackedWidget.setCurrentIndex(1)
+        
+    
+    def play_as_white_clicked(self):
+        self.chosen_color = PlayerColor.WHITE
+        self.ui.stackedWidget.setCurrentIndex(1)
+        
+    
+    def play_as_black_clicked(self):
+        self.chosen_color = PlayerColor.BLACK
         self.ui.stackedWidget.setCurrentIndex(1)
