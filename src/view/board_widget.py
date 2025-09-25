5e2e8ceac43881b33086f5d14702cb95178b44be
Michal Žatečka <zateckamichal8@gmail.com>
2025-09-25 10:51:45 +0200

    make pieces movement without game logic

--- src/view/board_widget.py
+++ src/view/board_widget.py
@@ -1,6 +1,6 @@
-from PySide6.QtCore import Qt, QRectF
+from PySide6.QtCore import Qt, QRect, QRectF, QPointF
 from PySide6.QtGui import QPixmap, QColor, QPainterPath, QBrush
-from PySide6.QtWidgets import QWidget, QGraphicsScene, QGraphicsItem, QGraphicsPixmapItem, QGraphicsRectItem
+from PySide6.QtWidgets import QWidget, QGraphicsScene, QGraphicsItem, QGraphicsPixmapItem, QGraphicsRectItem, QGraphicsItemGroup
 
 from src.view.ui.board_widget_ui import Ui_BoardWidget
 from src.constants import PlayerColor
@@ -73,38 +73,79 @@ class BoardWidget(QWidget):
             black_piece = ChessPiece(f"img/black_{piece}.png", self.SQUARE_SIZE)
             black_pawn = ChessPiece("img/black_pawn.png", self.SQUARE_SIZE)
 
-            # set their starting position
-            if self.down_player == PlayerColor.WHITE:
-                black_piece.setOffset(self.SQUARE_SIZE * col, 0)
-                black_pawn.setOffset(self.SQUARE_SIZE * col, self.SQUARE_SIZE)
-                white_piece.setOffset(self.SQUARE_SIZE * col, (self.BOARD_SIZE-1)*self.SQUARE_SIZE)
-                white_pawn.setOffset(self.SQUARE_SIZE * col, (self.BOARD_SIZE-2)*self.SQUARE_SIZE)
-            else:
-                white_piece.setOffset(self.SQUARE_SIZE * col, 0)
-                white_pawn.setOffset(self.SQUARE_SIZE * col, self.SQUARE_SIZE)
-                black_piece.setOffset(self.SQUARE_SIZE * col, (self.BOARD_SIZE-1)*self.SQUARE_SIZE)
-                black_pawn.setOffset(self.SQUARE_SIZE * col, (self.BOARD_SIZE-2)*self.SQUARE_SIZE)
-
             # add them to scene
             self.scene.addItem(white_piece)
             self.scene.addItem(white_pawn)
             self.scene.addItem(black_pawn)
             self.scene.addItem(black_piece)
 
+            # set their starting position
+            if self.down_player == PlayerColor.WHITE:
+                black_piece.setPos(black_piece.mapFromScene(self.SQUARE_SIZE * col, 0))
+                black_pawn.setPos(black_pawn.mapFromScene(self.SQUARE_SIZE * col, self.SQUARE_SIZE))
+                white_piece.setPos(white_piece.mapFromScene(self.SQUARE_SIZE * col, (self.BOARD_SIZE-1)*self.SQUARE_SIZE))
+                white_pawn.setPos(white_pawn.mapFromScene(self.SQUARE_SIZE * col, (self.BOARD_SIZE-2)*self.SQUARE_SIZE))
+            else:
+                white_piece.setPos(white_piece.mapFromScene(self.SQUARE_SIZE * col, 0))
+                white_pawn.setPos(white_pawn.mapFromScene(self.SQUARE_SIZE * col, self.SQUARE_SIZE))
+                black_piece.setPos(black_piece.mapFromScene(self.SQUARE_SIZE * col, (self.BOARD_SIZE-1)*self.SQUARE_SIZE))
+                black_pawn.setPos(black_pawn.mapFromScene(self.SQUARE_SIZE * col, (self.BOARD_SIZE-2)*self.SQUARE_SIZE))
 
 
-class ChessPiece(QGraphicsRectItem):
-    def __init__(self, img_path, square_size, parent=None):
-        super().__init__(0, 0, square_size, square_size, parent)
-        self.setBrush(QBrush(QColor(0, 0, 0, 0)))
-        self.setFlag(QGraphicsRectItem.ItemIsMovable, True)
+
+class ChessPiece(QGraphicsPixmapItem):
+    def __init__(self, img_path, square_size):
+        pixmap = PieceQPixmap(img_path, square_size).scaled(square_size, square_size)
+        super().__init__(pixmap)
+        self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsMovable)
+        self.setShapeMode(self.ShapeMode.BoundingRectShape)
+        self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemSendsGeometryChanges)
+        self.old_pos = None
         self.square_size = square_size
 
-        pixmap = QPixmap(img_path).scaled(square_size, square_size)
-        self.piece = QGraphicsPixmapItem(pixmap, self)
-        pw, ph = pixmap.width(), pixmap.height()
-        self.piece.setPos((square_size - pw) / 2, (square_size - ph) / 2)
     
+    def mousePressEvent(self, event):
+        # save piece's start position for invalid move
+        self.old_pos = self.pos()
+        return super().mousePressEvent(event)
 
-    def setOffset(self, x, y):
-        self.setRect(x, y, self.square_size, self.square_size)
+
+    # TODO return to old pos for invalid moves
+    def mouseReleaseEvent(self, event):
+        point = self.scenePos()
+        # calculate closest square to piece position when dropped
+        new_x = abs(round(point.x() / self.square_size)) * self.square_size
+        new_y = abs(round(point.y() / self.square_size)) * self.square_size
+        self.setPos(new_x, new_y)
+        self.old_pos = None
+        super().mouseReleaseEvent(event)
+
+
+    def itemChange(self, change, value):
+        # make pieces not to leave chess board
+        if change == QGraphicsItem.GraphicsItemChange.ItemPositionChange and self.scene():
+            new_pos = value
+            scene_rect = self.scene().sceneRect()
+            br = self.boundingRect()
+
+            x = min(
+                max(new_pos.x(), scene_rect.left()),
+                scene_rect.right() - br.height()
+            )
+            y = min(
+                max(new_pos.y(), scene_rect.top()),
+                scene_rect.bottom() - br.height()
+            )
+            return QPointF(x, y)
+        return super().itemChange(change, value)
+
+
+
+class PieceQPixmap(QPixmap):
+    def __init__(self, img_path, square_size):
+        super().__init__(img_path)
+        self.square_size = square_size
+
+    
+    def rect(self) -> QRect:
+        return QRect(0, 0, self.square_size, self.square_size)
